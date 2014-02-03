b258b1cda1517045562ff192988da0f635fca567
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2014-02-03 13:26:57 -0500

    deleted files

--- comp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-
-urlpatterns = patterns('comp.views',
-	url(r'^$', 'home'),
-	url(r'^logout$', 'logout_view'),
-	url(r'^sign_up$', 'sign_up'),
-
-	)
