61daad7f769006f636004ae2f6904e760cc50fea
tom creighton <tjcreighton@gmail.com>
2014-02-04 16:12:18 -0500

    added medbiq xml parsing

--- xci/views.py
+++ xci/views.py
@@ -1,5 +1,23 @@
-from xci import app
+from xci import app, competency
+from flask import make_response, request
+import json
 
 @app.route('/')
 def index():
-    return 'yay we dids it! <br>DEBIG: %s <br>SECRET: %s' % (app.config['DEBUG'], app.config['SECRET_KEY'])
+    uri = request.args.get('uri', None)
+    if uri:
+        p = competency.parseMedBiq(uri)
+        try:
+            resp = make_response(json.dumps(p), 200)
+            resp.headers['Content-Type'] = "application/json"
+            return resp
+        except Exception as e:
+            return make_response("%s<br>%s" % (str(e), p), 200)
+            # return make_response("fail <br> %s" % repr(p), 200)
+
+    return '''yay we dids it! 
+              <br>DEBUG: %s 
+              <br>SECRET: %s
+              <br><a href="./?uri=http://adlnet.gov/competency-framework/scorm/choosing-an-lms.xml">choose lms</a>
+              <br><a href="./?uri=http://adlnet.gov/competency-framework/computer-science/basic-programming.xml">programming</a>
+              <br><a href="./?uri=http://12.109.40.34/performance-framework/xapi/tetris.xml">perf tetris</a>''' % (app.config['DEBUG'], app.config['SECRET_KEY'])
