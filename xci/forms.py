e46e1dfdd62732471b3f69ad0937193e4e54aad7
Lou Wolford <lou.wolford.ctr@adlnet.gov>
2014-02-06 12:15:05 -0500

    added model for login and error messages

--- xci/forms.py
+++ xci/forms.py
@@ -1,17 +1,60 @@
 from flask_wtf import Form
 from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired
+from pymongo import MongoClient
+from werkzeug.security import check_password_hash
+
+mongo = MongoClient()
+db = mongo.xci
 
 class LoginForm(Form):
 	username = TextField('Username', validators=[DataRequired()])
 	password = PasswordField('Password', validators=[DataRequired()])
 
+	def __init__(self, *args, **kwargs):
+		Form.__init__(self, *args, **kwargs)
+		self.user = None
+
+	def validate(self):
+		rv = Form.validate(self)
+		if not rv:
+			return False
+
+		user = db.userprofiles.find_one({'username':self.username.data})
+		if user is None:
+			self.username.errors.append('Unknown username')
+			return False
+
+		if not check_password_hash(user['password'],self.password.data):
+			self.password.errors.append('Invalid password')
+			return False
+
+		self.user = user
+		return True
+
 class RegistrationForm(Form):
-	username = TextField('Username', validators=[DataRequired()])
-	password = PasswordField('Password', validators=[DataRequired()])
 	first_name = TextField('First Name', validators=[DataRequired()])
 	last_name = TextField('Last Name', validators=[DataRequired()])
 	email = TextField('Email', validators=[DataRequired()])
+	username = TextField('Username', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired()])
+
+	def __init__(self, *args, **kwargs):
+		Form.__init__(self, *args, **kwargs)
+		self.user = None
+
+	def validate(self):
+		rv = Form.validate(self)
+		if not rv:
+			return False
+
+		user = db.userprofiles.find_one({'username':self.username.data})
+		if user:
+			self.username.errors.append('Username already exists')
+			return False
+
+		self.user = user
+		return True	
 
 class FrameworksForm(Form):
 	framework_uri = TextField('Framework URI', validators=[DataRequired()])
--- xci/views.py
+++ xci/views.py
@@ -1,25 +1,34 @@
 from xci import app, competency
 from flask import render_template, redirect, flash, url_for, request, make_response
 from forms import LoginForm, RegistrationForm, FrameworksForm
-from flask_login import LoginManager, login_user
+from models import User
+from flask_login import LoginManager, login_user, login_required, logout_user
+from pymongo import MongoClient
+from werkzeug.security import generate_password_hash
 import json
 
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+mongo = MongoClient()
+db = mongo.xci
 
 def add_login_to_return_dict(r_dict):
 	r_dict['login_form'] = LoginForm()
 	return r_dict
 
 @login_manager.user_loader
-def load_user(userid):
-	# return User.get(userid)
-	# this is where we would grab the user from the db
-	return true
+def load_user(user):
+	if isinstance(user, basestring):
+		userobj = User(user, 'get')
+		u_id = userobj.get_id()
+		return userobj
+	else:
+		u_id = user.get_id()
+		return user
 
 @app.route('/', methods=['GET'])
 def index():
-    # return 'yay we dids it! <br>DEBIG: %s <br>SECRET: %s' % (app.config['DEBUG'], app.config['SECRET_KEY'])
     return_dict = add_login_to_return_dict({})
     return render_template('home.html', **return_dict)
     # uri = request.args.get('uri', None)
@@ -41,26 +50,49 @@ def index():
     #           <br><a href="./?uri=http://12.109.40.34/performance-framework/xapi/tetris.xml">perf tetris</a>''' % (app.config['DEBUG'], app.config['SECRET_KEY'])
 
 
+@app.route('/logout')
+@login_required
+def logout():
+	logout_user()
+	return redirect(url_for('index'))
 
 @app.route('/login', methods=["POST"])
 def login():
-	form = LoginForm()
-	if form.validate_on_submit():
-		# login_user(user)
-		return redirect(request.args.get("next") or url_for("index"))
+	lf = LoginForm(request.form)
+	next = request.args.get("next")
+
+	if lf.validate_on_submit():
+		user = User(lf.username.data, generate_password_hash(lf.password.data))
+		login_user(user)
+		return redirect(next or url_for("index"))
+	else:
+		import pdb
+		pdb.set_trace()
+		if next == '/':
+			return render_template('home.html',login_form=lf)
+		elif next == '/frameworks':
+			return render_template('frameworks.html', login_form=lf, frameworks_form=FrameworksForm(), cfwks=competency.get_all_comp_frameworks())
+		elif next == '/sign_up':
+			return render_template('sign_up', login_form=lf, signup_form=RegistrationForm())
+		elif next == '/me':
+			return render_template('me', login_form=lf)
 
 @app.route('/sign_up', methods=["GET", "POST"])
 def sign_up():
-
 	if request.method == 'GET':
-		return_dict = {'signup_form': SystemUserForm(), 'hide': True}
+		return_dict = {'signup_form': RegistrationForm(), 'hide': True}
 		return render_template('sign_up.html', **add_login_to_return_dict(return_dict))
 	else:
-		rf = RegistrationForm()
-		#validate form somehow
-		#save user to db
-		#log user in
-		return redirect(url_for('index'))
+		rf = RegistrationForm(request.form)
+		if rf.validate_on_submit():
+			users = db.userprofiles
+			users.insert({'username': rf.username.data, 'password':generate_password_hash(rf.password.data), 'email':rf.email.data,
+				'first_name':rf.first_name.data, 'last_name':rf.last_name.data, 'competencies':[], 'compfwks':[], 'lrsprofiles':[]})
+			
+			user = User(rf.username.data, generate_password_hash(rf.password.data))
+			login_user(user)
+			return redirect(request.args.get('next') or url_for('index'))
+		return render_template(url_for('sign_up'), rf)
 
 
 @app.route('/frameworks', methods=["GET", "POST"])
@@ -69,7 +101,7 @@ def frameworks():
 	if request.method == 'GET':
 		return_dict['cfwks'] = competency.get_all_comp_frameworks()
 	else:
-		ff = FrameworksForm(request.POST)
+		ff = FrameworksForm(request.form)
 		#validate form here somehow
 		try:
 			#add to system
@@ -80,6 +112,7 @@ def frameworks():
 
 
 @app.route('/me', methods=["GET"])
+@login_required
 def me():
 	return render_template('me.html')
 
